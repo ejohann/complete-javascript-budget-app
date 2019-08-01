5e5e618af7b89a799054cd543959eb35c8b30a01
Johanne <johanne_lewis@hotmail.com>
2019-08-01 14:26:39 +0100

    Replace placeholders with values

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ var UIController = (function(){
         
         addListItem: function(obj, type){
             
-            var html;
+            var html, newHtml;
             
             // create HTML string with placeholder text
             if(type === 'inc'){
@@ -96,9 +96,12 @@ var UIController = (function(){
             else if(type === 'exp'){
               html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';   
              }
-       
-            
+                   
             // replace the placeholder text
+            
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%descritption%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
             
             // insert the HTML into the DOM
             
