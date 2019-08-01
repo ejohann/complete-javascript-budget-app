ab26e6f789b17e9014759c57243e0a9f3cd32c48
Johanne <johanne_lewis@hotmail.com>
2019-08-01 06:28:22 +0100

    Delete unnecessary code - separate main parts

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
+/*************************************
+*** BUDGET CONTROLLER
+**************************************/
 var budgetController = (function(){
-    var x = 23;
-    var add = function(a){
-        return x + a;
-    }
-  
-    return{
-        publicTest: function(b){
-            return (add(b));
-        }
-    }
+ // some code
 })();
 
+
+/**************************************
+*** UI CONTROLLER
+**************************************/
 var UIController = (function(){
     //some code
 })();
 
 
+/******************************************
+*** GLOBAL APP CONTROLLER
+******************************************/
 var controller = (function(budgetControl, UIControl){
-    var z = budgetControl.publicTest(5);
-    
-    return{
-        anotherPublic: function(){
-            console.log(z);
-        }
-    }
+ 
 })(budgetController, UIController);
