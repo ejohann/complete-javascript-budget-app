df46985f51967356e1276d69824abb1120454cb4
Johanne <johanne_lewis@hotmail.com>
2019-08-01 11:51:25 +0100

    Create an initialiser function for event listeners

--- app.js
+++ app.js
@@ -38,13 +38,20 @@ var UIController = (function(){
 ******************************************/
 var controller = (function(budgetControl, UIControl){
  
-    var DOM = UIControl.getDOMstrings();
-    
+    var setupEventListeners = function(){
+         var DOM = UIControl.getDOMstrings();
+         document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+         document.addEventListener('keypress', function(event){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+             } 
+          });
+    };
+      
     var ctrlAddItem = function(){
         // 1.  Get the field input data
         var input = UIControl.getInput();
-        
-        
+         
         // 2. Add the item to the budget controller
         
         // 3. Calculate the budget
@@ -54,14 +61,15 @@ var controller = (function(budgetControl, UIControl){
         // 5. Display the bidget on the UI
         
         console.log(input);
-    }
+    };
     
-    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-    
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode === 13 || event.which === 13){
-           ctrlAddItem();
-         } 
-    });
+  return{
+      init: function(){
+          console.log('Aplication has started');
+          setupEventListeners();
+      }
+  }  
     
 })(budgetController, UIController);
+
+controller.init();
