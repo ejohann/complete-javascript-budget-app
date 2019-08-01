ef882f5b0b9319e6e64c126eab5f2f31cabd2046
Johanne <johanne_lewis@hotmail.com>
2019-08-01 12:07:17 +0100

    Create function contructors for income and expense

--- app.js
+++ app.js
@@ -3,6 +3,18 @@
 **************************************/
 var budgetController = (function(){
  // some code
+ var Expense = function(id, description, value){
+     this.id = id;
+     this.description = description;
+     this.value = value;
+ };   
+   
+ var Income = function(id, description, value){
+     this.id = id;
+     this.description = description;
+     this.value = value;
+ };
+    
 })();
 
 
